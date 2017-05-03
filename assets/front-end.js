a4e8536ce24162e327d7997ef35e95c247bfdd2a
Kuntao Zhu <kuntao1220@gmail.com>
2017-05-03 15:46:04 -0400

    test
    just testing

---
+++ assets/front-end.js
@@ -0,0 +1,58 @@
+var app = angular.module('myapp',['ngRoute']);
+app.config(function($routeProvider){
+	$routeProvider.when('/login',{
+	     templateUrl:'login.html',
+	     controller:'logincontroller'
+	})
+	.when('/home',{
+	     templateUrl:'home.html',
+	     controller:'homecontroller',
+	})
+	.when('/signup',{
+	     templateUrl:'signup.html',
+	     controller:'signupcontroller',		
+	})
+	.when('/checkout',{
+	     templateUrl:'checkout.html',
+	     controller:'checkoutcontroller',		
+	})
+	.when('/cart',{
+		 templateUrl:'cart.html',
+	     controller:'cartcontroller',	
+	})
+	.otherwise({redirectTo:'/login'});
+	
+});
+app.factory('customefactory',['$scope','$http','$log',function($scope,$http,$log){
+	var customeservice = {};    
+	customeservice.login = function(customer){
+		$http.post('/login',customer)
+		.success(function(rep){
+			//alert(rep.name);
+			    //console.log(rep);
+		        if(rep==='success'){
+		        	$location.path('/home');
+		        }
+		        else{
+		        	alert('login failed');
+		        }
+
+				
+			
+		})
+	};
+	return logservice;
+	
+	
+}])
+app.controller('signupcontroller',['$scope','$location',function($scope,$location){
+
+	
+}])
+app.controller('logincontroller',['$scope','$location','customefactory',function($scope,$location,customefactory){
+	var customer = {'username':$scope.username,'password':$scope.password};
+	$scope.login = function(){
+		customefactory.login(customer);
+	}
+	
+}])
---
+++ server.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var app = express();
+var mongodb = require('mongodb');
+var http = require('http');
+
+var mongoclient = require('mongodb').MongoClient;
+
+
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var mongodbstore = require('connect-mongodb-session')(session);
+
+/*var new_db = new mongodb.Db('aa', new mongodb.Server('localhost', 27017));
+new_db.open(function(err, db){
+	if(!err){
+		db.collection("aaa").insert({"test":"2323"});
+	}
+})*/
+
+var conn_str = 'mongodb://localhost:27017/example';
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+var store = new mongodbstore({
+	uri:'mongodb://localhost:27017/example',
+	collection:'example_session'
+	});
+app.use(
+		session({
+			secret:'example_sess_secret_key',
+			cookie:{maxAge:1000*60*2}
+			resave:true,
+			saveUninitialized:true,
+			store:store
+		})
+		);
+store.on('error',function(err){
+	console.log(err);
+});
+app.get('/',function(req,res){
+	res.sendFile(__dirname + "/index.html");
+})
+app.post('/login',function(req.res){
+	mongoclient.connect(conn_str,function(err,db){
+		if(err){
+			console.log('error occur');
+		}else{
+			db.collection('customers').findOne({"username":req.body.username,"password":req.body.password},function(err,result){
+				console.log(result);
+				if(result){
+					req.session.loguser = result;
+					res.send('success');
+					
+				}else{
+					res.send('failed');
+				}
+				db.close();
+			})
+			
+		}
+	})
+	
+})
