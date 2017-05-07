2a17afa4280dee0a678716cbb95d4dea063fab25
Kuntao Zhu <kuntao1220@gmail.com>
2017-05-07 13:45:13 -0400

    test6
    add more pages

--- assets/front-end.js
+++ assets/front-end.js
@@ -16,6 +16,22 @@ app.config(function($routeProvider){
 	     templateUrl:'cart.html',
 	     controller:'cartcontroller',		
 	})
+	.when('/cn',{
+		templateUrl:'cn.html',
+		controller:'cncontroller',
+	})
+	.when('/us',{
+		templateUrl:'us.html',
+		controller:'uscontroller',
+	})
+	.when('/detailcn',{
+	  templateUrl:'detailcn.html',
+		controller:'detailcncontroller',
+	})
+	.when('/detailus',{
+	  templateUrl:'detailus.html',
+		controller:'detailuscontroller',
+	})
 	.otherwise({redirectTo:'/home'});
 	
 });
@@ -68,5 +84,96 @@ app.controller('checkoutcontroller',['$scope','$location','customefactory',funct
 	}
 	
 }])
+
+  app.controller('cncontroller',['$scope','$http','$location','$rootScope',function($scope,$http,$location,$rootScope){
+
+		  $http.get("https://api.nike.com/commerce/productfeed/products/snkrs/threads?country=CN&limit=500&locale=zh_CN&withCards=true")
+		  .then(function(rep){
+			  $scope.threads = rep.data.threads;
+			  /*console.log(rep.data.threads)*/
+
+			  
+		  })
+		  
+		  $scope.view = function(id){
+		    /*console.log(id);*/
+		    $http.get("https://api.nike.com/commerce/productfeed/products/snkrs/" + id + "/thread?country=CN&locale=zh_CN&withCards=true")
+		    .then(function(rep){
+		      console.log(rep.data);
+/*		      $scope.available = rep.data.product.skus.available;
+		      $scope.size = rep.data.product.skus.localizedSize;*/
+		      console.log(rep.data.id);
+		      $rootScope.stylenumber = id;
+		      $rootScope.price = rep.data.product.price.currentRetailPrice;
+		      $rootScope.onsale = rep.data.product.price.onSale;
+		      $rootScope.sizeinfo = rep.data.product.skus;
+		      $rootScope.fullTitle = rep.data.product.fullTitle;
+		      $rootScope.effectiveInStockStartSellDate = rep.data.product.effectiveInStockStartSellDate;
+		      $rootScope.link = 'https://www.nike.com/cn/launch/thread/'+rep.data.id;
+		      $rootScope.merchStatus = rep.data.product.merchStatus;
+		      $rootScope.selectionEngine = rep.data.product.selectionEngine;
+		      $rootScope.estimatedLaunchDate = rep.data.product.estimatedLaunchDate;
+		      $rootScope.startSellDate = rep.data.product.startSellDate;
+		      $rootScope.waitinline = rep.data.product.waitlineEnabled;
+		      $rootScope.image = rep.data.desktopImageUrl;
+		      $location.path('/detailcn');
+		      
+		    })
+		  };
+		  
+	  
+	  
+  }]);
+
+app.controller('detailcncontroller',['$scope','$http','$location','$rootScope',function($scope,$http,$location,$rootScope){
+      $scope.link = $rootScope.link;
+  
+}]);
+
+
+app.controller('uscontroller',['$scope','$http','$location','$rootScope',function($scope,$http,$location,$rootScope){
+
+		  $http.get("https://api.nike.com/commerce/productfeed/products/snkrs/threads?country=US&limit=500&locale=en_US&withCards=true")
+		  .then(function(rep){
+			  $scope.threads = rep.data.threads;
+			  /*console.log(rep.data.threads)*/
+
+			  
+		  })
+		  
+		  $scope.view = function(id){
+		    /*console.log(id);*/
+		    $http.get("https://api.nike.com/commerce/productfeed/products/snkrs/" + id + "/thread?country=US&locale=en_US&withCards=true")
+		    .then(function(rep){
+		      console.log(rep.data);
+/*		      $scope.available = rep.data.product.skus.available;
+		      $scope.size = rep.data.product.skus.localizedSize;*/
+		      console.log(rep.data.id);
+		      $rootScope.stylenumber = id;
+		      $rootScope.price = rep.data.product.price.currentRetailPrice;
+		      $rootScope.onsale = rep.data.product.price.onSale;
+		      $rootScope.sizeinfo = rep.data.product.skus;
+		      $rootScope.fullTitle = rep.data.product.fullTitle;
+		      $rootScope.effectiveInStockStartSellDate = rep.data.product.effectiveInStockStartSellDate;
+		      $rootScope.link = 'https://www.nike.com/cn/launch/thread/'+rep.data.id;
+		      $rootScope.merchStatus = rep.data.product.merchStatus;
+		      $rootScope.selectionEngine = rep.data.product.selectionEngine;
+		      $rootScope.estimatedLaunchDate = rep.data.product.estimatedLaunchDate;
+		      $rootScope.startSellDate = rep.data.product.startSellDate;
+		      $rootScope.waitinline = rep.data.product.waitlineEnabled;
+		      $rootScope.image = rep.data.desktopImageUrl;
+		      $location.path('/detailus');
+		      
+		    })
+		  };
+		  
+	  
+	  
+  }]);
+
+app.controller('detailuscontroller',['$scope','$http','$location','$rootScope',function($scope,$http,$location,$rootScope){
+      $scope.link = $rootScope.link;
+  
+}]);
 
 
